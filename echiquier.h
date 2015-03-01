b52e18df6530ca5ab1106a66cedb812adb7ed6ef
kaeria <inesguo93@gmail.com>
2015-03-01 22:02:38 +0100

    Update echiquier.h

--- echiquier.h
+++ echiquier.h
@@ -1,20 +1,22 @@
 #ifndef ECHIQUIER_H_INCLUDED
 #define ECHIQUIER_H_INCLUDED
+#include "definition.h"
+#include "piece.h"
+#include "position.h"
 
 class echiquier
 {
     public:
     int dim;
-    piece **echiquier;
-    bool casevide(int l,int c)
+    piece **echectab; //le tableau 8*8 de pointeurs de pieces
+    bool casevide(int l,int c) //prend une case de coordonee(l,c) et regarde si la case est vide
     void print();
-    echiquier();
-    echiquier(const echiquier &);
-    echiquier & operator=(const operator &);
+    echiquier(); //initialisation d'un echiquier
+    echiquier(const echiquier &); //operateur par copie
+    echiquier & operator=(const operator &); //operateur =
     ~echiquier();
 
 
 };
-
 
 #endif // ECHIQUIER_H_INCLUDED
